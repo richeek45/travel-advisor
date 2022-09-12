d953e14c99ad077edb1d183dd0a7d5442c4c741e
Richeek45 <richeek45@gmail.com>
2022-09-12 18:30:31 +0530

    not usable due to invalid api api_key because google cloud doesn't accept debit cards

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,79 @@
+import { AppBar, Box, CircularProgress, InputBase, Toolbar, Typography } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
 import React from 'react'
+import { GoogleMap, LoadScript, Autocomplete, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
+
+const API_KEY = "AIzaSyDM2ZU1AqSOzokO6ATWVaXFWi3QXCVrdU4"
+
+// const options = {
+//     zoomControlOptions: {
+//         position: window.google.maps.ControlPosition.RIGHT_CENTER // 'right-center' ,
+//         // ...otherOptions
+//     }
+// }
+
+export const MapLoader = () => {
+    const { isLoaded, loadError } = useJsApiLoader({
+        googleMapsApiKey: API_KEY
+    })
+
+    // const onLoad = useCallback(function onLoad(mapInstance) {
+    //     // do something with map Instance
+    // })
+
+
+    return (
+        <LoadScript
+            googleMapsApiKey={API_KEY}
+            libraries={['places']}
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            // onLoad={onLoad}
+            >
+                <></>
+            </GoogleMap>
+        </LoadScript>
+    )
+}
+
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <LoadScript
+            googleMapsApiKey={API_KEY}
+            libraries={['places']}
+        >
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography>Travel Advisor</Typography>
+                </Toolbar>
+                <Box display="flex">
+                    <Typography>Explore new places</Typography>
+                    <Autocomplete>
+                        {/* <div>
+                        <div>
+                            <SearchIcon />
+                        </div>
+                    </div> */}
+                        <InputBase />
+                    </Autocomplete>
+                </Box>
+
+            </AppBar>
+        </LoadScript>
     )
 }
 
